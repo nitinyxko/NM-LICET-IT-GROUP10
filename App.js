e68f80f9e6ed0157127874921721418faca9667c
nitinyxko <144327581+nitinyxko@users.noreply.github.com>
2023-11-22 21:20:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,68 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Login from "./Login.js";
+import Signup from "./Signup.js";
+import Today from "./Today.js";
+import Profile from "./Profile.js";
+import Exercise from "./Exercise.js";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      result: ''
-    };
-  }
+const Navigator = createStackNavigator({
+  Login: { screen: Login },
+  Signup: { screen: Signup },
+  Today: { screen: Today },
+  Profile: { screen: Profile },
+  Exercise: { screen: Exercise },
+});
 
-  handleButtonClick = (value) => {
-    if (value === '=') {
-      try {
-        this.setState({
-          input: this.state.result,
-          result: eval(this.state.result)
-        });
-      } catch (error) {
-        this.setState({
-          input: 'Error',
-          result: ''
-        });
-      }
-    } else if (value === 'C') {
-      this.setState({
-        input: '',
-        result: ''
-      });
-    } else {
-      this.setState({
-        input: this.state.input + value,
-        result: this.state.result + value
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="calculator">
-          <input type="text" value={this.state.input} readOnly />
-          <div className="buttons">
-            <button onClick={() => this.handleButtonClick('7')}>7</button>
-            <button onClick={() => this.handleButtonClick('8')}>8</button>
-            <button onClick={() => this.handleButtonClick('9')}>9</button>
-            <button onClick={() => this.handleButtonClick('+')}>+</button>
-            <button onClick={() => this.handleButtonClick('4')}>4</button>
-            <button onClick={() => this.handleButtonClick('5')}>5</button>
-            <button onClick={() => this.handleButtonClick('6')}>6</button>
-            <button onClick={() => this.handleButtonClick('-')}>-</button>
-            <button onClick={() => this.handleButtonClick('1')}>1</button>
-            <button onClick={() => this.handleButtonClick('2')}>2</button>
-            <button onClick={() => this.handleButtonClick('3')}>3</button>
-            <button onClick={() => this.handleButtonClick('*')}>*</button>
-            <button onClick={() => this.handleButtonClick('C')}>C</button>
-            <button onClick={() => this.handleButtonClick('0')}>0</button>
-            <button onClick={() => this.handleButtonClick('=')}>=</button>
-            <button onClick={() => this.handleButtonClick('/')}>/</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = createAppContainer(Navigator);
 
 export default App;
